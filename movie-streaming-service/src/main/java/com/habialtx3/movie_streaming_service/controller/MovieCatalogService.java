c6dc005103242f8b1a2087bae135a896ef14bd90
Muhammad Reza Pahlevi <rezaworku31@gmail.com>
2026-05-12 11:26:20 +0700

    feat : feature stream by movie info id

---
+++ movie-streaming-service/src/main/java/com/habialtx3/movie_streaming_service/controller/MovieCatalogService.java
@@ -0,0 +1,18 @@
+package com.habialtx3.movie_streaming_service.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class MovieCatalogService {
+
+    public static  final String CATALOG_SERVICE = "http://movie-catalogue-service";
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public String getMoviePath(Long movieInfoId) {
+        var response = restTemplate.getForEntity(CATALOG_SERVICE + "/movie-info/find-path-by-id/{movieInfoId}",String.class,movieInfoId);
+        return response.getBody();
+    }
+}
--- movie-streaming-service/src/main/java/com/habialtx3/movie_streaming_service/controller/MovieStreamController.java
+++ movie-streaming-service/src/main/java/com/habialtx3/movie_streaming_service/controller/MovieStreamController.java
@@ -1,6 +1,7 @@
 package com.habialtx3.movie_streaming_service.controller;
 
 import org.apache.http.entity.InputStreamEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,17 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 public class MovieStreamController {
+
+    private static final Logger log = Logger.getLogger(MovieStreamController.class.getName());
+
+    @Autowired
+    private MovieCatalogService movieCatalogService;
 
     public static String VIDEO_DIRECTORY = "C:/Users/Acer/Videos/";
 
     @GetMapping("/stream/{videoPath}")
     public ResponseEntity<InputStreamResource> streamVideo(@PathVariable String videoPath) throws FileNotFoundException {
-        File file = new File(VIDEO_DIRECTORY+videoPath);
+        File file = new File(VIDEO_DIRECTORY + videoPath);
 
-        if(file.exists()){
+        if (file.exists()) {
             InputStreamResource inputStreamResource = new InputStreamResource(new FileInputStream(file));
             return ResponseEntity.ok()
                     .contentType(MediaType.parseMediaType("video/mp4"))
@@ -30,5 +38,12 @@ public class MovieStreamController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping("/stream/with-id/{movieInfoId}")
+    public ResponseEntity<InputStreamResource> streamVideo(@PathVariable Long movieInfoId) throws FileNotFoundException {
+        String moviePath = movieCatalogService.getMoviePath(movieInfoId);
+        log.log(Level.INFO, "Resolver movie path : {0}",moviePath);
+        return streamVideo(moviePath);
     }
 }
